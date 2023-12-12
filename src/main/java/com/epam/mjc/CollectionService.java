ab209a9911f713cff30211f9093abf02d0503cbd
Yerke Nurgali <70740293+qelephant@users.noreply.github.com>
2023-12-12 10:48:22 +0600

    Update CollectionService.java

--- src/main/java/com/epam/mjc/CollectionService.java
+++ src/main/java/com/epam/mjc/CollectionService.java
@@ -6,31 +6,39 @@ import java.util.stream.Collectors;
 
 public class CollectionService {
 
+    // Task 1: Stream Filter
     public List<Integer> filterEvenNumbers(List<Integer> list) {
         return list.stream()
                 .filter(number -> number % 2 == 0)
                 .collect(Collectors.toList());
     }
 
+    // Task 2: Stream Map
     public List<String> toUpperCaseCollection(List<String> list) {
         return list.stream()
-                .filter(s -> s.length() > 3)
+                .map(String::toUpperCase)
                 .collect(Collectors.toList());
     }
 
+    // Task 3: Stream Max
     public Optional<Integer> findMax(List<Integer> list) {
         return list.stream()
                 .max(Integer::compareTo);
     }
 
+    // Task 4: Stream FlatMap
     public Optional<Integer> findMin(List<List<Integer>> list) {
         return list.stream()
                 .flatMap(List::stream)
-                .max(Integer::compareTo);
+                .min(Integer::compareTo);
     }
 
+    // Task 5: Stream Reduce
     public Integer sum(List<Integer> list) {
         return list.stream()
                 .reduce(0, Integer::sum);
     }
 }
+
+
+
